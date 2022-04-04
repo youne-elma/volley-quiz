a75cf226fd53705647c2ffba3176a34f3217f57a
Youness El Marhoum <youne.elma@gmail.com>
2022-04-04 03:30:40 +0000

    fetch the json api and add the game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,82 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Loading from '../components/Loading'
+
+// import PNGS
+import blop3 from '../images/blop3.png'
+import blop4 from '../images/blop4.png'
+
+
 
 const Game = () => {
+
+  const [loading,setLoading] = useState(false);
+  const [questions,setQuestions] = useState([]);
+
+  const url = 'https://opentdb.com/api.php?amount=10';
+                  // -----------------------OPTION1-------------
+                  // let displayData
+                  // function pullJson() {
+                  //   setLoading(true);
+                  //   fetch(url)
+                  //   .then(response => response.json())
+                  //   .then(responseData => {
+                  //     displayData = responseData.map(item => {
+                  //       return(
+                  //         <p>{item}</p>
+                  //       )
+                  //     })
+                  //     console.log(responseData)
+                  //     setQuestions(displayData);
+                  //   })
+                  //   //return
+                  //   setLoading(false);
+                  // }
+
+  //OPTION 2
+  async function pullJson(){
+    setLoading(true);
+    try{
+      const response = await fetch(url);
+      const responseData = await response.json();
+
+      if(responseData.results){
+        console.log(responseData.results)
+        setQuestions(responseData.results);
+      }else{
+        setQuestions([]);  
+      }
+      setLoading(false);
+    }catch(error){
+      console.log(error);
+      setLoading(false)
+    }
+  }
+
+  
+  useEffect(() => {
+    pullJson();
+  }, [url])
+
+
   return (
-    <div>Game</div>
+    <div className="main">
+        <img className="blop3" src={blop3} alt="pictureBlop"/>
+        <div className="questions">
+          {loading && <Loading />}
+          {questions.map(item => {
+            return(
+              <h1>
+                {item.difficulty}
+              </h1>
+            )
+          })}
+        </div>
+        <div className="result">
+          {/* <p>Your result is 3/5 Nice one!</p> */}
+          <button className="checkAnswer">Check the answers</button>
+        </div>
+        <img className="blop4" src={blop4} alt="pictureBlop" />
+    </div>
   )
 }
 
--- src/pages/StartGame.js
+++ src/pages/StartGame.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+// import { Spring } from 'react-spring';
 // import SVGS
 import blop1 from '../images/blop1.png'
 import blop2 from '../images/blop2.png'
@@ -9,17 +10,21 @@ import volleyballIcon from '../images/volleyball-icon.png'
 
 const StartGame = () => {
   return (
-    <div className="main">
-        <img className="blop1" src={blop1} alt="blop1"/>
-        <img className="blop2" src={blop2} alt="blop2"/>
-        <div className="main-text">
-            <h1 className="title-quiz">Volleyball Quiz</h1>
-            <p className="parag-quiz">a quiz about volleyball,<br /> hurry up and start the quiz</p>
-            <Link to="/game" className="start-button">start the quiz</Link>
-        </div>
-        <img className="spike-image" src={spikeImage} alt="spike volleyball"/>
-        <img className="volleyball-icon" src={volleyballIcon} alt="volleyball ball"/>
-    </div>
+    // <Spring
+    //   from={{opacity: 0}}
+    //   to={{opacity: 1}}
+    // >
+      <div className="main">
+          <img className="blop1" src={blop1} alt="blop1"/>
+          <img className="blop2" src={blop2} alt="blop2"/>
+          <div className="main-text">
+              <h1 className="title-quiz">Volleyball Quiz</h1>
+              <p className="parag-quiz">a quiz about volleyball,<br /> hurry up and start the quiz</p>
+              <Link to="/game" className="start-button">start the quiz</Link>
+          </div>
+          <img className="spike-image" src={spikeImage} alt="spike volleyball"/>
+          <img className="volleyball-icon" src={volleyballIcon} alt="volleyball ball"/>
+      </div>
   )
 }
 
